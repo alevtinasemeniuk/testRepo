3a4977c4a39d570cd5164832f599287c1a00062c
Alevtina Semeniuk <166370278+alevtinasemeniuk@users.noreply.github.com>
2024-11-02 12:45:36 -0700

    As/new branch update class (#5)
    * update test
    
    * edit ImportantJob.txt
    
    * edit ImportantJob.txt
    
    * edit ImportantJob.txt
    
    * update test

--- src/test/java/PracticeTest.java
+++ src/test/java/PracticeTest.java
@@ -16,27 +16,31 @@ public class PracticeTest {
      private WebDriver driver;
 
     @BeforeMethod
+
     public void setUp() throws InterruptedException {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
     }
 
     @Test
-    public void testMainCheckbox() throws InterruptedException {
+    public void testMainCheckbox()  {
+
         driver.get("https://demoqa.com/checkbox");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement mainCheckbox = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='rct-checkbox']")));
         mainCheckbox.click();
-        Thread.sleep(1000);
-        mainCheckbox.click();
 
         List<WebElement> childChekboxes = driver.findElements(By.xpath("//input[@type='checkbox']"));
+
 
         for (WebElement checkbox : childChekboxes){
 
             Assert.assertTrue(checkbox.isSelected());
+
         }
         driver.quit();
+
     }
 
 }
