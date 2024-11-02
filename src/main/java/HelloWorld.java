c477cc873f5d9d95f2aad681d2400d15bf365a9d
alevtinasemeniuk <alewtinni@gmail.com>
2024-11-02 13:02:06 -0700

    update class

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,13 @@
+import java.util.List;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
 
         System.out.println("Hello, world!");
+
+        List<String> words = List.of("В", "траве", "сидел", "кузнечик");
+        System.out.println(words);
 
     }
 }
